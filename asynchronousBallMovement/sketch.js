12122add6ddd6716f1bbdaabc5078a55f23ae91f
nirvan7 <75612989+nirvan7@users.noreply.github.com>
2021-04-01 12:54:43 +0530

    Add files via upload

---
+++ asynchronousBallMovement/sketch.js
@@ -0,0 +1,20 @@
+var database; 
+var game;
+var gameState=0;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+ game=new Game();;
+game.getState();
+game.start
+}
+
+
+function draw(){
+  
+}
+
